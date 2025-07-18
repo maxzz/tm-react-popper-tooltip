08528f3a59200f53a8390b9e51d65afc2b51d20d
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-18 15:42:35 -0700

    Initial commit

---
+++ src/usePopperTooltip.ts
@@ -0,0 +1,305 @@
+import type {CSSProperties} from 'react';
+import {useMemo, useState, useRef, useEffect, useCallback} from 'react';
+import type {VirtualElement} from '@popperjs/core';
+import {usePopper} from 'react-popper';
+import {
+  useControlledState,
+  useGetLatest,
+  generateBoundingClientRect,
+} from './utils.js';
+import type {
+  Config,
+  PopperOptions,
+  PropsGetterArgs,
+  TriggerType,
+} from './types.js';
+
+const virtualElement: VirtualElement = {
+  getBoundingClientRect: generateBoundingClientRect(),
+};
+
+const defaultConfig: Config = {
+  closeOnOutsideClick: true,
+  closeOnTriggerHidden: false,
+  defaultVisible: false,
+  delayHide: 0,
+  delayShow: 0,
+  followCursor: false,
+  interactive: false,
+  mutationObserverOptions: {
+    attributes: true,
+    childList: true,
+    subtree: true,
+  },
+  offset: [0, 6],
+  trigger: 'hover',
+};
+
+export function usePopperTooltip(
+  config: Config = {},
+  popperOptions: PopperOptions = {},
+) {
+  // Merging options with default options.
+  // Keys with undefined values are replaced with the default ones if any.
+  // Keys with other values pass through.
+  const finalConfig = (
+    Object.keys(defaultConfig) as Array<keyof typeof defaultConfig>
+  ).reduce(
+    (config, key) => ({
+      ...config,
+      [key]: config[key] !== undefined ? config[key] : defaultConfig[key],
+    }),
+    config,
+  );
+
+  const defaultModifiers = useMemo(
+    () => [{name: 'offset', options: {offset: finalConfig.offset}}],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    Array.isArray(finalConfig.offset) ? finalConfig.offset : [],
+  );
+
+  const finalPopperOptions = {
+    ...popperOptions,
+    placement: popperOptions.placement || finalConfig.placement,
+    modifiers: popperOptions.modifiers || defaultModifiers,
+  };
+
+  const [triggerRef, setTriggerRef] = useState<HTMLElement | null>(null);
+  const [tooltipRef, setTooltipRef] = useState<HTMLElement | null>(null);
+  const [visible, setVisible] = useControlledState({
+    initial: finalConfig.defaultVisible,
+    value: finalConfig.visible,
+    onChange: finalConfig.onVisibleChange,
+  });
+
+  const timer = useRef<number>();
+  useEffect(() => () => clearTimeout(timer.current), []);
+
+  const {styles, attributes, ...popperProps} = usePopper(
+    finalConfig.followCursor ? virtualElement : triggerRef,
+    tooltipRef,
+    finalPopperOptions,
+  );
+
+  const update = popperProps.update;
+
+  const getLatest = useGetLatest({
+    visible,
+    triggerRef,
+    tooltipRef,
+    finalConfig,
+  });
+
+  const isTriggeredBy = useCallback(
+    (trigger: TriggerType) => {
+      return Array.isArray(finalConfig.trigger)
+        ? finalConfig.trigger.includes(trigger)
+        : finalConfig.trigger === trigger;
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    Array.isArray(finalConfig.trigger)
+      ? finalConfig.trigger
+      : [finalConfig.trigger],
+  );
+
+  const hideTooltip = useCallback(() => {
+    clearTimeout(timer.current);
+    timer.current = window.setTimeout(
+      () => setVisible(false),
+      finalConfig.delayHide,
+    );
+  }, [finalConfig.delayHide, setVisible]);
+
+  const showTooltip = useCallback(() => {
+    clearTimeout(timer.current);
+    timer.current = window.setTimeout(
+      () => setVisible(true),
+      finalConfig.delayShow,
+    );
+  }, [finalConfig.delayShow, setVisible]);
+
+  const toggleTooltip = useCallback(() => {
+    if (getLatest().visible) {
+      hideTooltip();
+    } else {
+      showTooltip();
+    }
+  }, [getLatest, hideTooltip, showTooltip]);
+
+  // Handle click outside
+  useEffect(() => {
+    if (!getLatest().finalConfig.closeOnOutsideClick) return;
+
+    const handleClickOutside: EventListener = (event) => {
+      const {tooltipRef, triggerRef} = getLatest();
+      const target = event.composedPath?.()?.[0] || event.target;
+      if (target instanceof Node) {
+        if (
+          tooltipRef != null &&
+          triggerRef != null &&
+          !tooltipRef.contains(target) &&
+          !triggerRef.contains(target)
+        ) {
+          hideTooltip();
+        }
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [getLatest, hideTooltip]);
+
+  // Trigger: click
+  useEffect(() => {
+    if (triggerRef == null || !isTriggeredBy('click')) return;
+
+    triggerRef.addEventListener('click', toggleTooltip);
+
+    return () => triggerRef.removeEventListener('click', toggleTooltip);
+  }, [triggerRef, isTriggeredBy, toggleTooltip]);
+
+  // Trigger: double-click
+  useEffect(() => {
+    if (triggerRef == null || !isTriggeredBy('double-click')) return;
+
+    triggerRef.addEventListener('dblclick', toggleTooltip);
+
+    return () => triggerRef.removeEventListener('dblclick', toggleTooltip);
+  }, [triggerRef, isTriggeredBy, toggleTooltip]);
+
+  // Trigger: right-click
+  useEffect(() => {
+    if (triggerRef == null || !isTriggeredBy('right-click')) return;
+
+    const preventDefaultAndToggle: EventListener = (event) => {
+      // Don't show the context menu
+      event.preventDefault();
+      toggleTooltip();
+    };
+
+    triggerRef.addEventListener('contextmenu', preventDefaultAndToggle);
+    return () =>
+      triggerRef.removeEventListener('contextmenu', preventDefaultAndToggle);
+  }, [triggerRef, isTriggeredBy, toggleTooltip]);
+
+  // Trigger: focus
+  useEffect(() => {
+    if (triggerRef == null || !isTriggeredBy('focus')) return;
+
+    triggerRef.addEventListener('focus', showTooltip);
+    triggerRef.addEventListener('blur', hideTooltip);
+    return () => {
+      triggerRef.removeEventListener('focus', showTooltip);
+      triggerRef.removeEventListener('blur', hideTooltip);
+    };
+  }, [triggerRef, isTriggeredBy, showTooltip, hideTooltip]);
+
+  // Trigger: hover on trigger
+  useEffect(() => {
+    if (triggerRef == null || !isTriggeredBy('hover')) return;
+
+    triggerRef.addEventListener('mouseenter', showTooltip);
+    triggerRef.addEventListener('mouseleave', hideTooltip);
+    return () => {
+      triggerRef.removeEventListener('mouseenter', showTooltip);
+      triggerRef.removeEventListener('mouseleave', hideTooltip);
+    };
+  }, [triggerRef, isTriggeredBy, showTooltip, hideTooltip]);
+
+  // Trigger: hover on tooltip, keep it open if hovered
+  useEffect(() => {
+    if (
+      tooltipRef == null ||
+      !isTriggeredBy('hover') ||
+      !getLatest().finalConfig.interactive
+    )
+      return;
+
+    tooltipRef.addEventListener('mouseenter', showTooltip);
+    tooltipRef.addEventListener('mouseleave', hideTooltip);
+    return () => {
+      tooltipRef.removeEventListener('mouseenter', showTooltip);
+      tooltipRef.removeEventListener('mouseleave', hideTooltip);
+    };
+  }, [tooltipRef, isTriggeredBy, showTooltip, hideTooltip, getLatest]);
+
+  // Handle closing tooltip if trigger hidden
+  const isReferenceHidden =
+    popperProps?.state?.modifiersData?.hide?.isReferenceHidden;
+  useEffect(() => {
+    if (finalConfig.closeOnTriggerHidden && isReferenceHidden) hideTooltip();
+  }, [finalConfig.closeOnTriggerHidden, hideTooltip, isReferenceHidden]);
+
+  // Handle follow cursor
+  useEffect(() => {
+    if (!finalConfig.followCursor || triggerRef == null) return;
+
+    function setMousePosition({
+      clientX,
+      clientY,
+    }: {
+      clientX: number;
+      clientY: number;
+    }) {
+      virtualElement.getBoundingClientRect = generateBoundingClientRect(
+        clientX,
+        clientY,
+      );
+      update?.();
+    }
+
+    triggerRef.addEventListener('mousemove', setMousePosition);
+    return () => triggerRef.removeEventListener('mousemove', setMousePosition);
+  }, [finalConfig.followCursor, triggerRef, update]);
+
+  // Handle tooltip DOM mutation changes (aka mutation observer)
+  useEffect(() => {
+    if (
+      tooltipRef == null ||
+      update == null ||
+      finalConfig.mutationObserverOptions == null
+    )
+      return;
+
+    const observer = new MutationObserver(update);
+    observer.observe(tooltipRef, finalConfig.mutationObserverOptions);
+    return () => observer.disconnect();
+  }, [finalConfig.mutationObserverOptions, tooltipRef, update]);
+
+  // Tooltip props getter
+  const getTooltipProps = (args: PropsGetterArgs = {}) => {
+    return {
+      ...args,
+      style: {
+        ...args.style,
+        ...styles['popper'],
+      } as CSSProperties,
+      ...attributes['popper'],
+      'data-popper-interactive': finalConfig.interactive,
+    };
+  };
+
+  // Arrow props getter
+  const getArrowProps = (args: PropsGetterArgs = {}) => {
+    return {
+      ...args,
+      ...attributes['arrow'],
+      style: {
+        ...args.style,
+        ...styles['arrow'],
+      } as CSSProperties,
+      'data-popper-arrow': true,
+    };
+  };
+
+  return {
+    getArrowProps,
+    getTooltipProps,
+    setTooltipRef,
+    setTriggerRef,
+    tooltipRef,
+    triggerRef,
+    visible,
+    ...popperProps,
+  };
+}
---
+++ tests/usePopperTooltip.spec.tsx
@@ -0,0 +1,272 @@
+import '@testing-library/jest-dom/vitest';
+import {beforeEach, afterEach, describe, test, vi, expect} from 'vitest';
+import {render, screen, waitFor, fireEvent} from '@testing-library/react';
+import {userEvent} from '@testing-library/user-event';
+import {usePopperTooltip, Config} from '../src/main.js';
+
+const TriggerText = 'Trigger';
+const TooltipText = 'Tooltip';
+
+function Tooltip({options}: {options: Config}) {
+  const {
+    setTriggerRef,
+    setTooltipRef,
+    getArrowProps,
+    getTooltipProps,
+    visible,
+  } = usePopperTooltip(options);
+
+  return (
+    <>
+      <button ref={setTriggerRef}>{TriggerText}</button>
+
+      {visible && (
+        <div
+          ref={setTooltipRef}
+          {...getTooltipProps()}
+        >
+          <div {...getArrowProps()} />
+          {TooltipText}
+        </div>
+      )}
+    </>
+  );
+}
+
+beforeEach(() => {
+  vi.useFakeTimers();
+});
+
+afterEach(async () => {
+  await vi.runOnlyPendingTimersAsync();
+  vi.useRealTimers();
+});
+
+const user = userEvent.setup({
+  advanceTimers: vi.advanceTimersByTime,
+});
+
+describe('trigger option', () => {
+  test('hover trigger', async () => {
+    render(<Tooltip options={{trigger: 'hover'}} />);
+
+    // tooltip not visible initially
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+
+    // tooltip shown on hover in
+    user.hover(screen.getByText(TriggerText));
+    expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+
+    // tooltip hidden on hover out
+    user.unhover(screen.getByText(TriggerText));
+    await waitFor(() => {
+      expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+    });
+  });
+
+  test('click trigger', async () => {
+    render(<Tooltip options={{trigger: 'click'}} />);
+
+    // tooltip not visible initially
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+
+    // tooltip shown on click
+    user.click(screen.getByText(TriggerText));
+    expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+
+    // tooltip hidden on click
+    user.click(screen.getByText(TriggerText));
+    await waitFor(() => {
+      expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+    });
+  });
+
+  test('right-click trigger', async () => {
+    render(<Tooltip options={{trigger: 'right-click'}} />);
+
+    // tooltip not visible initially
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+
+    // tooltip shown on right-click
+    user.pointer({
+      keys: '[MouseRight>]',
+      target: screen.getByText(TriggerText),
+    });
+    expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+
+    // tooltip hidden on right-click
+    user.pointer({
+      keys: '[MouseRight>]',
+      target: screen.getByText(TriggerText),
+    });
+    await waitFor(() => {
+      expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+    });
+  });
+
+  test('focus trigger', async () => {
+    render(<Tooltip options={{trigger: 'focus'}} />);
+
+    // tooltip not visible initially
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+
+    // tooltip shown on focus
+    user.tab();
+    expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+
+    // tooltip hidden on blur
+    user.tab();
+    await waitFor(() => {
+      expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+    });
+  });
+
+  test('trigger array', async () => {
+    render(<Tooltip options={{trigger: ['click', 'hover']}} />);
+
+    // tooltip not visible initially
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+
+    // tooltip shown on hover
+    user.hover(screen.getByText(TriggerText));
+    expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+
+    // tooltip hidden on click
+    user.click(screen.getByText(TriggerText));
+    await waitFor(() => {
+      expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+    });
+  });
+
+  test('null trigger', async () => {
+    render(<Tooltip options={{trigger: null}} />);
+
+    // tooltip not visible initially
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+
+    // Nothing after hover
+    user.hover(screen.getByText(TriggerText));
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+
+    // Nothing after click
+    user.click(screen.getByText(TriggerText));
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+
+    // Nothing after right-click
+    fireEvent.contextMenu(screen.getByText(TriggerText));
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+
+    // Nothing after focus
+    fireEvent.focus(screen.getByText(TriggerText));
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+  });
+});
+
+test('closeOnOutsideClick removes tooltip on document.body click', async () => {
+  render(<Tooltip options={{trigger: 'click'}} />);
+
+  // Show on click
+  user.click(screen.getByText(TriggerText));
+  expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+
+  // Hide on body click
+  user.click(document.body);
+  await waitFor(() => {
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+  });
+});
+
+test("closeOnOutsideClick doesn't remove tooltip on tooltip click", async () => {
+  render(<Tooltip options={{trigger: 'click'}} />);
+
+  // Show on click
+  user.click(screen.getByText(TriggerText));
+  expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+
+  user.click(screen.getByText(TooltipText));
+  await waitFor(() => {
+    expect(screen.queryByText(TooltipText)).toBeInTheDocument();
+  });
+});
+
+test('delayShow option renders tooltip after specified delay', async () => {
+  render(<Tooltip options={{delayShow: 5000}} />);
+
+  user.hover(screen.getByText(TriggerText));
+  // Nothing after a 2000ms
+  await vi.advanceTimersByTimeAsync(2000);
+  expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+
+  await vi.runAllTimersAsync();
+  // It shows up sometime later. Here RTL uses fake timers to await as well, so
+  // it awaits for the element infinitely, advancing jest fake timer by 50ms
+  // in an endless loop. And this is why the test passes even if delayShow set
+  // a way bigger than a default timeout of 1000ms would allow.
+  expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+});
+
+test('delayHide option removes tooltip after specified delay', async () => {
+  render(<Tooltip options={{delayHide: 5000}} />);
+
+  user.hover(screen.getByText(TriggerText));
+  await vi.runAllTimersAsync();
+  expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+
+  user.unhover(screen.getByText(TriggerText));
+  // Still present after 2000ms
+  await vi.advanceTimersByTimeAsync(2000);
+  expect(screen.getByText(TooltipText)).toBeInTheDocument();
+  await vi.runAllTimersAsync();
+  // Removed some time later
+  expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+});
+
+describe('defaultVisible option', () => {
+  test('with false value renders nothing', async () => {
+    render(<Tooltip options={{defaultVisible: false}} />);
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+  });
+
+  test('with true value renders tooltip', async () => {
+    render(<Tooltip options={{defaultVisible: true}} />);
+    expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+  });
+});
+
+test('onVisibleChange option called when state changes', async () => {
+  const onVisibleChange = vi.fn();
+  render(<Tooltip options={{onVisibleChange}} />);
+
+  // By default not visible, change visible to true when first time hover
+  user.hover(screen.getByText(TriggerText));
+  expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+  expect(onVisibleChange).toHaveBeenLastCalledWith(true);
+
+  // Now visible, change visible to false when unhover
+  user.unhover(screen.getByText(TriggerText));
+  await waitFor(() => {
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+  });
+  expect(onVisibleChange).toHaveBeenLastCalledWith(false);
+  expect(onVisibleChange).toHaveBeenCalledTimes(2);
+});
+
+describe('visible option controls the state and', () => {
+  test('with false value renders nothing', async () => {
+    render(<Tooltip options={{visible: false, trigger: 'click'}} />);
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+
+    // The state is controlled, click doesn't change it
+    user.click(screen.getByText(TriggerText));
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+  });
+
+  test('with true value renders tooltip', async () => {
+    render(<Tooltip options={{visible: true, trigger: 'click'}} />);
+    expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+
+    // The state is controlled, click doesn't change it
+    user.click(screen.getByText(TriggerText));
+    expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+  });
+});
