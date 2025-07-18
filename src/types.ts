1541a423d319019c0fc3b98344498a8d14fc7993
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-18 16:11:54 -0700

    Initial code clean up

--- src/types.ts
+++ src/types.ts
@@ -1,85 +1,85 @@
-import {CSSProperties} from 'react';
-import {Placement, Options, createPopper} from '@popperjs/core';
+import { CSSProperties } from 'react';
+import { Placement, Options, createPopper } from '@popperjs/core';
 
 export type TriggerType =
-  | 'click'
-  | 'double-click'
-  | 'right-click'
-  | 'hover'
-  | 'focus';
+    | 'click'
+    | 'double-click'
+    | 'right-click'
+    | 'hover'
+    | 'focus';
 
 export type Config = {
-  /**
-   * Whether to close the tooltip when its trigger is out of boundary
-   * @default false
-   */
-  closeOnTriggerHidden?: boolean;
-  /**
-   * Event or events that trigger the tooltip
-   * @default hover
-   */
-  trigger?: TriggerType | TriggerType[] | null;
-  /**
-   * Delay in hiding the tooltip (ms)
-   * @default 0
-   */
-  delayHide?: number;
-  /**
-   * Delay in showing the tooltip (ms)
-   * @default 0
-   */
-  delayShow?: number;
-  /**
-   * Whether to make the tooltip spawn at cursor position
-   * @default false
-   */
-  followCursor?: boolean;
-  /**
-   * Options to MutationObserver, used internally for updating
-   * tooltip position based on its DOM changes
-   * @default  { attributes: true, childList: true, subtree: true }
-   */
-  mutationObserverOptions?: MutationObserverInit | null;
-  /**
-   * Whether tooltip is shown by default
-   * @default false
-   */
-  defaultVisible?: boolean;
-  /**
-   * Used to create controlled tooltip
-   */
-  visible?: boolean;
-  /**
-   * Called when the visibility of the tooltip changes
-   */
-  onVisibleChange?: (state: boolean) => void;
-  /**
-   * If `true`, a click outside the trigger element closes the tooltip
-   * @default true
-   */
-  closeOnOutsideClick?: boolean;
-  /**
-   * If `true`, hovering the tooltip will keep it open. Normally tooltip closes when the mouse cursor moves out of
-   * the trigger element. If it moves to the tooltip element, the tooltip stays open.
-   * @default false
-   */
-  interactive?: boolean;
-  /**
-   * Alias for popper.js placement, see https://popper.js.org/docs/v2/constructors/#placement
-   */
-  placement?: Placement;
-  /**
-   * Shorthand for popper.js offset modifier, see https://popper.js.org/docs/v2/modifiers/offset/
-   * @default [0, 6]
-   */
-  offset?: [number, number];
+    /**
+     * Whether to close the tooltip when its trigger is out of boundary
+     * @default false
+     */
+    closeOnTriggerHidden?: boolean;
+    /**
+     * Event or events that trigger the tooltip
+     * @default hover
+     */
+    trigger?: TriggerType | TriggerType[] | null;
+    /**
+     * Delay in hiding the tooltip (ms)
+     * @default 0
+     */
+    delayHide?: number;
+    /**
+     * Delay in showing the tooltip (ms)
+     * @default 0
+     */
+    delayShow?: number;
+    /**
+     * Whether to make the tooltip spawn at cursor position
+     * @default false
+     */
+    followCursor?: boolean;
+    /**
+     * Options to MutationObserver, used internally for updating
+     * tooltip position based on its DOM changes
+     * @default  { attributes: true, childList: true, subtree: true }
+     */
+    mutationObserverOptions?: MutationObserverInit | null;
+    /**
+     * Whether tooltip is shown by default
+     * @default false
+     */
+    defaultVisible?: boolean;
+    /**
+     * Used to create controlled tooltip
+     */
+    visible?: boolean;
+    /**
+     * Called when the visibility of the tooltip changes
+     */
+    onVisibleChange?: (state: boolean) => void;
+    /**
+     * If `true`, a click outside the trigger element closes the tooltip
+     * @default true
+     */
+    closeOnOutsideClick?: boolean;
+    /**
+     * If `true`, hovering the tooltip will keep it open. Normally tooltip closes when the mouse cursor moves out of
+     * the trigger element. If it moves to the tooltip element, the tooltip stays open.
+     * @default false
+     */
+    interactive?: boolean;
+    /**
+     * Alias for popper.js placement, see https://popper.js.org/docs/v2/constructors/#placement
+     */
+    placement?: Placement;
+    /**
+     * Shorthand for popper.js offset modifier, see https://popper.js.org/docs/v2/modifiers/offset/
+     * @default [0, 6]
+     */
+    offset?: [number, number];
 };
 
 export type PopperOptions = Partial<Options> & {
-  createPopper?: typeof createPopper;
+    createPopper?: typeof createPopper;
 };
 
 export type PropsGetterArgs = {
-  style?: CSSProperties;
-  [key: string]: unknown;
+    style?: CSSProperties;
+    [key: string]: unknown;
 };
--- src/utils.ts
+++ src/utils.ts
@@ -1,65 +1,65 @@
-import {useRef, useCallback, useState} from 'react';
+import { useRef, useCallback, useState } from 'react';
 
 // kudos to @tannerlinsley https://twitter.com/tannerlinsley
 export function useGetLatest<T>(val: T) {
-  const ref = useRef<T>(val);
-  ref.current = val;
-  return useCallback(() => ref.current, []);
+    const ref = useRef<T>(val);
+    ref.current = val;
+    return useCallback(() => ref.current, []);
 }
 
 const noop = () => {
-  // do nothing
+    // do nothing
 };
 
 export function useControlledState<T>({
-  initial,
-  value,
-  onChange = noop,
+    initial,
+    value,
+    onChange = noop,
 }: {
-  initial?: T;
-  value?: T;
-  onChange?: (state: T) => void;
+    initial?: T;
+    value?: T;
+    onChange?: (state: T) => void;
 }): [T, (state: T) => void] {
-  if (initial === undefined && value === undefined) {
-    throw new TypeError(
-      'Either "value" or "initial" variable must be set. Now both are undefined',
+    if (initial === undefined && value === undefined) {
+        throw new TypeError(
+            'Either "value" or "initial" variable must be set. Now both are undefined',
+        );
+    }
+
+    const [state, setState] = useState(initial);
+
+    const getLatest = useGetLatest(state);
+
+    const set = useCallback(
+        (updater: T) => {
+            const state = getLatest();
+
+            const updatedState =
+                typeof updater === 'function' ? updater(state) : updater;
+
+            if (typeof updatedState.persist === 'function') updatedState.persist();
+
+            setState(updatedState);
+            if (typeof onChange === 'function') onChange(updatedState);
+        },
+        [getLatest, onChange],
     );
-  }
 
-  const [state, setState] = useState(initial);
+    const isControlled = value !== undefined;
 
-  const getLatest = useGetLatest(state);
-
-  const set = useCallback(
-    (updater: T) => {
-      const state = getLatest();
-
-      const updatedState =
-        typeof updater === 'function' ? updater(state) : updater;
-
-      if (typeof updatedState.persist === 'function') updatedState.persist();
-
-      setState(updatedState);
-      if (typeof onChange === 'function') onChange(updatedState);
-    },
-    [getLatest, onChange],
-  );
-
-  const isControlled = value !== undefined;
-
-  return [isControlled ? value! : state!, isControlled ? onChange : set];
+    return [isControlled ? value! : state!, isControlled ? onChange : set];
 }
 
 export function generateBoundingClientRect(x = 0, y = 0): () => DOMRect {
-  return () => ({
-    width: 0,
-    height: 0,
-    top: y,
-    right: x,
-    bottom: y,
-    left: x,
-    x: 0,
-    y: 0,
-    toJSON: () => null,
-  });
+    return () => ({
+        width: 0,
+        height: 0,
+        top: y,
+        right: x,
+        bottom: y,
+        left: x,
+        x: 0,
+        y: 0,
+        toJSON: () => null,
+    });
 }
